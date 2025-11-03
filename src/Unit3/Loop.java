3289c0875b87a76b439a649f998e3d5d16e411da
kfy200924-cpu <kfy200924@gmail.com>
2025-11-03 13:31:45 +0800

    Lab 2

---
+++ src/Unit3/Loop.java
@@ -0,0 +1,11 @@
+package Unit3;
+
+public class Loop {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <= 100) {
+            System.out.print("gernig");
+            count++;
+        }
+    }
+}
